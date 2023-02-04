26d6598014e9e198d26daab577d6c7f331d653a1
Keijiro Takahashi <keijiro@gmail.com>
2023-02-04 22:31:08 +0900

    Benchmark

--- Assets/Benchmark.cs
+++ Assets/Benchmark.cs
@@ -1,29 +1,41 @@
 using UnityEngine;
 using Debug = UnityEngine.Debug;
-using System.Diagnostics;
+using Stopwatch = System.Diagnostics.Stopwatch;
+using Type = System.Type;
 
 public sealed class Benchmark : MonoBehaviour
 {
     [SerializeField] int _instanceCount = 4096;
     [SerializeField] int _iterationCount = 128;
 
+    static readonly Type[] ComponentTypes = 
+      { typeof(TestComponent1), typeof(TestComponent2),
+        typeof(TestComponent3), typeof(TestComponent4),
+        typeof(TestComponent5), typeof(TestComponent6),
+        typeof(TestComponent7), typeof(TestComponent8) };
+
     void Start()
     {
-        var components = new []
-          { typeof(TestComponent1), typeof(TestComponent2),
-            typeof(TestComponent3), typeof(TestComponent4),
-            typeof(TestComponent5), typeof(TestComponent6),
-            typeof(TestComponent7), typeof(TestComponent8) };
+        var salt = Random.Range(0, _instanceCount - 1);
+        for (var i = 0; i < _instanceCount; i++) CreateGameObject(i ^ salt);
+        for (var i = 0; i < 8; i++) RunBenchmark(ComponentTypes[i]);
+    }
 
-        for (var i = 0; i < _instanceCount; i++)
-            new GameObject("Instance", components);
+    void CreateGameObject(int index)
+    {
+        var go = new GameObject("Instance");
+        for (var i = 0; i < 8; i++)
+            if ((index & ((1 << i) - 1)) == 0)
+                go.AddComponent(ComponentTypes[i]);
+    }
 
+    void RunBenchmark(Type type)
+    {
         var sw = new Stopwatch();
 
         sw.Start();
-        var sum = 0;
         for (var i = 0; i < _iterationCount; i++)
-            sum += FindObjectsOfType<TestComponent1>().Length;
+            FindObjectsOfType(type);
         sw.Stop();
 
         var time1 = sw.Elapsed.TotalMilliseconds;
@@ -31,13 +43,14 @@ public sealed class Benchmark : MonoBehaviour
         sw.Reset();
         sw.Start();
         for (var i = 0; i < _iterationCount; i++)
-            sum += FindObjectsByType<TestComponent1>(FindObjectsSortMode.None).Length;
+            FindObjectsByType(type, FindObjectsSortMode.None);
         sw.Stop();
 
         var time2 = sw.Elapsed.TotalMilliseconds;
 
-        Debug.Log($"FindObjectOfType: {time1:F2}");
-        Debug.Log($"FindObjectByType: {time2:F2}");
-        Debug.Log($"Gain: {1 - time2 / time1:P}");
+        var num = FindObjectsOfType(type).Length;
+        var ratio = 1 - time2 / time1;
+
+        Debug.Log($"{type} ({num}): {time1:F2} -> {time2:F2} ({ratio:P})");
     }
 }
