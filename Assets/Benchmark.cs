32664874e3d7888251622f5abd82c145efcb9363
Keijiro Takahashi <keijiro@gmail.com>
2023-02-06 14:13:18 +0900

    cull components

--- Assets/Benchmark.cs
+++ Assets/Benchmark.cs
@@ -3,10 +3,12 @@ using Debug = UnityEngine.Debug;
 using Stopwatch = System.Diagnostics.Stopwatch;
 using Type = System.Type;
 using System.Linq;
+using System.Collections;
 
 public sealed class Benchmark : MonoBehaviour
 {
-    [SerializeField] int _iterationCount = 64;
+    [SerializeField] int _instanceCount = 1024 * 8;
+    [SerializeField] int _iterationCount = 128;
 
     static readonly Type[] ComponentTypes = 
       { typeof(TestComponent1), typeof(TestComponent2),
@@ -14,13 +16,33 @@ public sealed class Benchmark : MonoBehaviour
         typeof(TestComponent5), typeof(TestComponent6),
         typeof(TestComponent7), typeof(TestComponent8) };
 
-    void Start()
+    IEnumerator Start()
     {
+        var list = Enumerable.Range(0, _instanceCount)
+          .Select(x => CreateFullGameObject(x)).ToList();
+
+        var salt = Random.Range(0, _instanceCount - 1);
+
+        for (var i = 0; i < _instanceCount; i++)
+            CullComponents(list[i], i ^ salt);
+
+        yield return null;
+
         var sheet = Enumerable.Range(0, 8)
           .Select(i => RunBenchmark(ComponentTypes[i]))
           .Aggregate((txt, line) => txt + "\n" + line);
 
         Debug.Log(sheet);
+    }
+
+    GameObject CreateFullGameObject(int index)
+      => new GameObject("Instance", ComponentTypes);
+
+    void CullComponents(GameObject go, int mask)
+    {
+        for (var i = 0; i < 8; i++)
+            if ((mask & ((1 << i) - 1)) != 0)
+                Destroy(go.GetComponent(ComponentTypes[7 - i]));
     }
 
     string RunBenchmark(Type type)
